30d5aa2b89156bd0728abf9412e8551ff499513e
Brendon Mota <bmota@its.jnj.com>
2021-07-03 12:24:23 -0300

    New Fonts

--- App.js
+++ App.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import Navigator from './src/navigation/Navigator';
-import { Provider as PaperProvider } from 'react-native-paper';
 import './src/config/Moment';
 
 export default function () {
   return (
-    <PaperProvider >
-      <Navigator />
-    </PaperProvider>
+    <Navigator />
   );
 };
