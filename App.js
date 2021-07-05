3d11216ac938e7f1fc62179c77e6ac4aa36864f5
Brendon Mota <bmota@its.jnj.com>
2021-07-05 10:56:09 -0300

    React-native-paper

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Navigator from './src/components/Navigator';
+import { Provider as PaperProvider } from 'react-native-paper';
 import './src/config/Moment';
 
 export default function () {
   return (
-    <Navigator />
+    <PaperProvider>
+      <Navigator />
+    </PaperProvider>
   );
 };
