bc7b8402794d2384d4dd4cd6905c723807b4ff6f
Brendon Mota <bmota@its.jnj.com>
2021-07-05 10:16:34 -0300

    Improving Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,23 @@ import moment from 'moment';
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
-export default function ({ title }) {
+export default function ({ title, children }) {
+
+  function renderChildren() {
+    return (
+      <View style={styles.rightContainer}>
+        {children}
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.description}>{moment(new Date).locale('pt-br').format('dddd, DD MMMM').toUpperCase()}</Text>
-      <Text style={styles.title}>{title}</Text>
+      <View style={styles.leftContainer}>
+        <Text style={styles.description}>{moment(new Date).locale('pt-br').format('dddd, DD MMMM').toUpperCase()}</Text>
+        <Text style={styles.title}>{title}</Text>
+      </View>
+      {children && renderChildren()}
     </View>
   );
 }
@@ -17,18 +29,24 @@ const styles = StyleSheet.create({
     borderBottomColor: '#000',
     borderBottomWidth: 0.2,
     elevation: 2,
+    flexDirection: 'row',
+    padding: 20,
   },
   description: {
     color: '#333',
-    margin: 20,
-    marginBottom: 0,
     fontFamily: 'Poppins-Thin',
   },
   title: {
-    margin: 20,
-    marginTop: 0,
     fontSize: 25,
     color: '#4a4a4a',
     fontFamily: 'AirbnbCereal-Medium',
+  },
+  leftContainer: {
+    flex: 1,
+  },
+  rightContainer: {
+    flex: 1,
+    alignItems: 'flex-end',
+    justifyContent: 'center',
   },
 });
