11cae28ad9e80958af5f7a070350d185dc20a79c
Brendon Mota <bmota@its.jnj.com>
2021-07-05 11:38:01 -0300

    Render back button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,30 @@
 import moment from 'moment';
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/Entypo';
 
-export default function ({ title, children, rowReverse }) {
+export default function ({ title, children, navigation }) {
 
   function renderChildren() {
     return (
-      <View style={{ ...styles.rightContainer, alignItems: rowReverse ? 'flex-start' : 'flex-end' }}>
+      <View style={styles.rightContainer}>
         {children}
-      </View>
+      </ View>
+    );
+  }
+
+  function renderBackButton() {
+    return (
+      <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
+        <Icon name="chevron-thin-left" size={25} color="#444" />
+      </TouchableOpacity>
     );
   }
 
   return (
-    <View style={{ ...styles.container, flexDirection: rowReverse ? 'row-reverse' : 'row' }}>
-      <View style={{ ...styles.leftContainer, alignItems: rowReverse ? 'flex-end' : 'flex-start' }}>
+    <View style={styles.container}>
+      {navigation && renderBackButton()}
+      <View style={styles.leftContainer}>
         <Text style={styles.description}>{moment(new Date).locale('pt-br').format('dddd, DD MMMM').toUpperCase()}</Text>
         <Text style={styles.title}>{title}</Text>
       </View>
@@ -29,7 +39,13 @@ const styles = StyleSheet.create({
     borderBottomColor: '#000',
     borderBottomWidth: 0.2,
     elevation: 2,
-    padding: 20,
+    padding: 15,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  backButton: {
+    marginRight: 10,
+    alignSelf: 'center',
   },
   description: {
     color: '#333',
@@ -46,5 +62,6 @@ const styles = StyleSheet.create({
   rightContainer: {
     flex: 1,
     justifyContent: 'center',
+    alignItems: 'flex-end',
   },
 });
