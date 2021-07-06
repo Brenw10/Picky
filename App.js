34250789a6f259cee548b8bf8eaa817256b87743
Brendon Mota <bmota@its.jnj.com>
2021-07-06 11:29:41 -0300

    Disable Dark Mode

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Navigator from './src/components/Navigator';
-import { Provider as PaperProvider } from 'react-native-paper';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 import './src/config/Moment';
 
 export default function () {
   return (
-    <PaperProvider>
+    <PaperProvider theme={DefaultTheme}>
       <Navigator />
     </PaperProvider>
   );
