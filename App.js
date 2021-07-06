8862b073a1b4a709e92ebffaadb980c998f43ade
Brendon Mota <bmota@its.jnj.com>
2021-07-06 14:39:52 -0300

    Default theme for react native paper

--- App.js
+++ App.js
@@ -3,9 +3,17 @@ import Navigator from './src/components/Navigator';
 import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 import './src/config/Moment';
 
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#0d46a1',
+  },
+};
+
 export default function () {
   return (
-    <PaperProvider theme={DefaultTheme}>
+    <PaperProvider theme={theme}>
       <Navigator />
     </PaperProvider>
   );
