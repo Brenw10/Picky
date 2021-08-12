492ba94b0213e22b6f7fa281a3afc8f3c1b20b4a
Brendon Mota <bmota@its.jnj.com>
2021-08-11 22:33:28 -0300

    Created component to list store products

---
+++ src/components/Products.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View, FlatList } from 'react-native';
+import ProductCard from '../components/ProductCard';
+import Product from '../api/Product';
+
+export default function ({ storeId, name, children, columns, height }) {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const query = { 'products.name': name };
+    Product.searchByStore(storeId, query).then(({ data }) => setProducts(data));
+  }, [search]);
+
+  function renderProducts({ item }) {
+    return (
+      <View style={{ ...styles.row, flex: 1 / columns }}>
+        <ProductCard product={item} height={height} />
+      </View>
+    );
+  }
+
+  return (
+    <>
+      <FlatList
+        data={products}
+        renderItem={renderProducts}
+        numColumns={columns}
+        keyExtractor={item => item._id}
+        ListHeaderComponent={children}
+      />
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  row: {
+    marginBottom: 10,
+  },
+});
--- src/screens/Store.js
+++ src/screens/Store.js
@@ -1,68 +1,32 @@
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, View, FlatList } from 'react-native';
+import React, { useState } from 'react';
 import Header from '../components/Header';
-import ProductCard from '../components/ProductCard';
-import SectionTitle from '../components/SectionTitle';
-import Searchbar from '../components/Searchbar';
-import StoreInformation from '../components/StoreInformation';
 import GoogleMaps from '../services/GoogleMaps';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import Product from '../api/Product';
-
-const COLUMNS = 3;
+import Products from '../components/Products';
+import StoreInformation from '../components/StoreInformation';
+import SectionTitle from '../components/SectionTitle';
+import Searchbar from '../components/Searchbar';
 
 export default function ({ route, navigation }) {
   const { store } = route.params;
-  const [products, setProducts] = useState([]);
   const [search, setSearch] = useState(route.params.search);
-
-  useEffect(() => {
-    const query = { 'products.name': search };
-    Product.searchByStore(store._id, query).then(({ data }) => setProducts(data));
-  }, [search]);
-
-  function renderProducts({ item }) {
-    return (
-      <View style={styles.row}>
-        <ProductCard product={item} height={180} />
-      </View>
-    );
-  }
-
-  function renderFlatListHeader() {
-    return (
-      <>
-        <StoreInformation store={store} />
-        <SectionTitle>Produtos</SectionTitle>
-        <Searchbar placeholder='Buscar Produtos' search={search} onSearch={setSearch} />
-      </>
-    );
-  }
 
   return (
     <>
       <Header title='Loja' navigation={navigation}>
-        <Icon.Button name="map-o" backgroundColor='rgba(0,0,0,0)' color='#4a4a4a'
-          underlayColor="transparent"
+        <Icon.Button name='map-o' backgroundColor='rgba(0,0,0,0)'
+          underlayColor='transparent' color='#4a4a4a'
           onPress={() => GoogleMaps.search(`${store.street},${store.number}`)}>
           Google Maps
         </Icon.Button>
       </Header>
-
-      <FlatList
-        data={products}
-        renderItem={renderProducts}
-        numColumns={COLUMNS}
-        keyExtractor={item => item._id}
-        ListHeaderComponent={renderFlatListHeader()}
-      />
+      <Products storeId={store._id} name={search} columns={3} height={180}>
+        <>
+          <StoreInformation store={store} />
+          <SectionTitle>Produtos</SectionTitle>
+          <Searchbar placeholder='Buscar Produtos' search={search} onSearch={setSearch} />
+        </>
+      </Products>
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  row: {
-    flex: 1 / COLUMNS,
-    marginBottom: 10,
-  },
-});
