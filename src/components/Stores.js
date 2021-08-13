fb85bdf10f7ed825ebc6765e077667e3028842d4
Brendon Mota <bmota@its.jnj.com>
2021-08-13 11:51:06 -0300

    Improving Stores Component

--- src/components/Stores.js
+++ src/components/Stores.js
@@ -16,25 +16,23 @@ export default function (props) {
     };
     Store.search(query).then(({ data }) => setStores(data));
   }, [props]);
-
-  function renderProducts(store) {
-    return store.products.map(product =>
-      <ProductCard
-        key={product._id}
-        product={product}
-        width={150} height={200}
-        onPress={() => onPress(store, product.name)}
-      />
-    );
-  }
-
+  
   return stores.map(store =>
     <View key={store._id}>
       <SectionSubtitle leftIcon='shop' onPress={() => onPress(store)}>
         {store.name}, {store.district}
       </SectionSubtitle>
       <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-        {renderProducts(store)}
+        {
+          store.products.map(product =>
+            <ProductCard
+              key={product._id}
+              product={product}
+              width={150} height={200}
+              onPress={() => onPress(store, product.name)}
+            />
+          )
+        }
       </ScrollView>
     </View>
   )
