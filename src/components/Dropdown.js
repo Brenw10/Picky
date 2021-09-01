fc71fbba4e86ef6a5a0c7fb2ffd0261698dab795
Brendon Mota <bmota@its.jnj.com>
2021-09-01 17:52:21 -0300

    Dropdown improve readability

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -10,30 +10,27 @@ export default function ({ items, setSelected, displayField, text }) {
     setVisible(false);
   }
 
-  function renderItems() {
-    return items.map(item =>
-      <Menu.Item
-        key={item._id}
-        onPress={() => onSelect(item)}
-        titleStyle={styles.item}
-        title={item[displayField]} />
-    );
-  }
-
-  function renderButton() {
-    return (
-      <Button color='#666' icon='chevron-down'
-        contentStyle={styles.buttonContainer}
-        labelStyle={styles.buttonText}
-        onPress={() => setVisible(true)}>
-        {text}
-      </Button>
-    );
-  }
-
   return (
-    <Menu visible={visible} onDismiss={() => setVisible(false)} anchor={renderButton()}>
-      {renderItems()}
+    <Menu
+      visible={visible}
+      onDismiss={() => setVisible(false)}
+      anchor={
+        <Button color='#666' icon='chevron-down'
+          contentStyle={styles.buttonContainer}
+          labelStyle={styles.buttonText}
+          onPress={() => setVisible(true)}>
+          {text}
+        </Button>
+      }>
+      {
+        items.map(item =>
+          <Menu.Item
+            key={item._id}
+            onPress={() => onSelect(item)}
+            titleStyle={styles.item}
+            title={item[displayField]} />
+        )
+      }
     </Menu>
   );
 }
